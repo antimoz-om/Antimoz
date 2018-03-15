9549e092d93d1e86d7fd415187a2f40c15a1eaf7
akhilcjacob <akhilcjacob@gmail.com>
2018-03-15 02:09:58 +0000

    Setup without docker containers

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# Download and install divolte
+curl -O http://divolte-releases.s3-website-eu-west-1.amazonaws.com/divolte-collector/0.7.0/distributions/divolte-collector-0.7.0.tar.gz
+tar -xzf divolte-collector-*.tar.gz
+rm -rf divolte-collector-*.tar.gz
+cd divolte-collector-*
+
+#
