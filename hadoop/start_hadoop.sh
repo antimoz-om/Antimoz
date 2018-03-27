a14d3c0ccaf8fc425f1a0b11b735e0d66fc19607
HaitianCapitalist <bdieu17@gmail.com>
2018-03-27 18:00:15 -0400

    Update start_hadoop.sh

--- hadoop/start_hadoop.sh
+++ hadoop/start_hadoop.sh
@@ -1,1 +1,1 @@
-docker run -d --name=hadoop -v /home/antimoz/project/hadoop/files:/files/ -p 6000:6000 docker.io/harisekhon/hadoop:latest
+docker run -d --name=hadoop -v ${HOME}/project/hadoop/files:/files/ -p 6000:6000 docker.io/harisekhon/hadoop:latest
