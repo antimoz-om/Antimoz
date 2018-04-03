84531e15cd41cfde6abba1e372d1939cdd9f517f
bdieu17@gmail.com <bdieu17@gmail.com>
2018-04-03 18:16:49 -0400

    Configuring the GSC file

---
+++ hadoop/mpr_jobs/main.py
@@ -0,0 +1,19 @@
+###########################
+
+# MapReduce Job in Python
+# Bring in Google Search console
+# & Google Analytics data
+
+# Mr-Job features
+
+###########################
+
+from mrjob.job import MRJob # MRJob library
+import re #regular expression library
+
+#raw string Notation --> r"text"
+
+
+# pull GSC data
+
+# can Mr-Job parse JSON if so then pipe into mapreduce job
