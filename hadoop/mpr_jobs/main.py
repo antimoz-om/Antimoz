43f7b257da94ea4a20d55ee7ba5603915c2e8f82
bdieu178 <bdieu17@gmail.com>
2018-04-07 14:42:07 -0400

    Added Hadoop Cheatsheet

--- hadoop/mpr_jobs/main.py
+++ hadoop/mpr_jobs/main.py
@@ -1,5 +1,7 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 ###################################
+
+# Author:  Bryan Dieudonne
 
 # MapReduce Job in Python
 # Bring in Google Search console
@@ -8,38 +10,63 @@
 # process the file with MR-Job
 # send o to HIVE data store
 
-# Mr-Job features
-
 ####################################
 
 from mrjob.job import MRJob        # MRJob library
 import re                          #regular expression library
 import gscquery
 import subprocess
+import os
 
-####################################
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
-
-
-####################################
-## Check if Hadoop is runnning
-##
-
-# hadoop_on = subprocess.run("hdfs ")
-if (!hadoop_on):
-    ## run this if hadoop is off
-else:
-    ## run this if hadoop is on
+def main():
+    ####################################
+    ## Check if Hadoop is runnning
+    bool hadoop_on = False
+    #add  do while to this after
+    while(!hadoop_on):
+        ## check hadoop
+        if this runs true <-- (subprocess.run(["hadoop version"]))
+        hadoop_on = True
+        break
 
     ####################################
-    ## Download Unsampled Google Search Console Data
-    ## onto the HDFS
     ##
+    if (!hadoop_on):
+        ## run this if hadoop is off
+    else:
+        ## run this if hadoop is on
+        program = "python ${HOME}/antimoz/hadoop/gscquery.py"
+        filepath = "${HOME}/antimoz/hadoop"
+        domain = "outspokenmedia.com"
+        start_date = "2018-01-01"
+        end_date = "2018-03-30"
 
-    #execute gscquery.main
-    #need to pass ENV varibals for arguments
-    bashCMD = "${HOME}/"
-    def push_GSC_onto_HDFS(gscquery.main(), bashCMD)
-        "save file to commane"
+        def push_GSC_onto_HDFS(program,URI,start_date,end_date):
+            """
+            Download Unsampled Google Search Console Data onto HDFS.
+            Parameters:
+            @Program
+            @URI
+            @start_date
+            @end_date
+            """
+            #launch bash CMD
+            subprocess.run([program,URI,start_date,end_date])
 
-        return
+            ## check if filepath exits
+                # if filepath does not exist create the file PATH
+
+            copyTohdfs = "hdfs f -CopyToLocal {file path..}" # fix this
+
+
+
+        #run hive command
+        subprocess.run()
+
+
+
+
+if __name__ == '__main__':
+    main()
